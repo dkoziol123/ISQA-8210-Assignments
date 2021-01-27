6882035af7ab0fdd2eeb865707ed810fcb91296a
dkoziol123 <70176289+dkoziol123@users.noreply.github.com>
2021-01-27 12:13:00 -0600

    Assignment 1

---
+++ clientms/clients/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Client
+
+
+class ClientAdmin(admin.ModelAdmin):
+    model = Client
+
+
+admin.site.register(Client, ClientAdmin)
+
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\owner\desktop\apps\2021sp\prototypes\clientms\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
