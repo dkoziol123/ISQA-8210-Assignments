f52627c494215713e5858698c5c24c8b6de607bf
dkoziol123 <70176289+dkoziol123@users.noreply.github.com>
2021-01-29 09:26:01 -0600

    Update settings.py

--- clientms/clientms/settings.py
+++ clientms/clientms/settings.py
@@ -111,4 +111,8 @@ LOGOUT_REDIRECT_URL = 'home'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/'
+
